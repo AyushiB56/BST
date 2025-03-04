eb16b1b813f557817389e56a046004b87c9f1668
AyushiB56 <157617227+AyushiB56@users.noreply.github.com>
2025-03-04 12:58:33 +0400

    Update bst_lookup.py

--- bst_lookup.py
+++ bst_lookup.py
@@ -1,21 +1,37 @@
 def lookup(root, value):
     current_node= root
     print(current_node.value)
-    while True:
+    while current_node is not None:
         if value < current_node.value:
-            if current_node.left is not None:
-                    current_node= current_node.left
-            elif current_node.value==value:
-                    return current_node
-            else:
-                    return "value not found"
-
+                current_node= current_node.left
 
         elif value > current_node.value:
-                if current_node.right is not None:
-                    current_node= current_node.right
-                elif current_node.value==value:
-                    return current_node
-                else:
-                    return "value not found"
+                 current_node= current_node.right
+        elif current_node.value==value:
+            return current_node
+    return None
+
+
+
+
+# def lookup(root, value):
+#     current_node= root
+#     print(current_node.value)
+#     while True:
+#         if value < current_node.value:
+#             if current_node.left is not None:
+#                     current_node= current_node.left
+#             elif current_node.value==value:
+#                     return current_node
+#             else:
+#                     return "value not found"
+
+
+#         elif value > current_node.value:
+#                 if current_node.right is not None:
+#                     current_node= current_node.right
+#                 elif current_node.value==value:
+#                     return current_node
+#                 else:
+#                     return "value not found"
 
