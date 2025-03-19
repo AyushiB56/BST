468a320ac8be1c73446e006ae0c6b52e12a0181a
AyushiB56 <157617227+AyushiB56@users.noreply.github.com>
2025-03-19 20:28:31 +0400

    Create node_depth.py

---
+++ node_depth.py
@@ -0,0 +1,41 @@
+#iteration
+def nodeDepths(root):
+    queue=[(root,0)]
+    current_depth=0
+
+    while len(queue)>0:
+        current_node,depth= queue.pop(0)
+        current_depth+=depth
+        if current_node.left:
+            queue.append((current_node.left,depth+1))
+        if current_node.right:
+            queue.append((current_node.right,depth+1))
+
+    return current_depth
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+
+#recursion
+def nodeDepths(root,depth=0):
+   if root is None:
+     return 0
+    left_depth= nodeDepths(root.left,depth+1)
+    right_depth=nodeDepths(root.right,depth+1)
+
+  return left_depth+right_depth
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+
+
